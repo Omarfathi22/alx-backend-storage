16a8c05b18228c45e2ba70ab57d412c5e3a708d8
Omarfathi22 <ommarf525@gmail.com>
2024-10-18 18:52:26 +0300

    MySQL

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
--- Rank country origins of bands by the number of fans
+-- Rank country origins of bands by the number of 
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
 SELECT origin, COUNT(*) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+    FROM metal_bands
+    BY origin
+    ORDER BY nb_fans DESC;
